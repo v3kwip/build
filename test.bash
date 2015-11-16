905c6d88b4b9c924b90f2fab5d88d28a50ab8bf2
Andy T <andytruong@users.noreply.github.com>
2015-11-16 16:29:18 +0700

    Create test.bash

---
+++ test.bash
@@ -0,0 +1,15 @@
+# --------
+# Test #accounts
+# --------
+cd ~/clone/accounts
+composer install
+./vendor/bin/phpunit
+
+# --------
+# Test #ui
+# --------
+cd ~/clone/ui
+grunt ngconstant:testing
+grunt build
+grunt jshint
+grunt test
