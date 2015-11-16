f63eac7f40a24686351c408dbd2cf2e0be275a00
Andy T <andytruong@users.noreply.github.com>
2015-11-16 15:25:53 +0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,12 +1,22 @@
-export CXX=g++-4.8
+# ---------------------
+# Caching
+# ---------------------
+COMPOSER_HOME=${HOME}/cache/composer
 
-nvm install 4 &&
-  npm install -g npm@latest &&
-  npm --version &&
-  npm install -g bower grunt-cli &&
-  gem --version &&
-  gem install sass compass guard-livereload
+mkdir -p ${HOME}/cache/drush/
+  && CACHE_PREFIX=${HOME}/cache/drush/
+  && export CACHE_PREFIX
 
+# npm
+export PREFIX="${HOME}/cache/npm/"
+export PATH="${HOME}/cache/npm/bin/:${PATH}"
+npm config set cache "${HOME}/cache/npm/"
+
+# ---------------------
+# PHP & Tools
+# ---------------------
+phpenv local 5.6
+export PATH="${HOME}/.composer/vendor/bin:${PATH}"
 composer config -g github-oauth.github.com ${GITHUB_OAUTH_TOKEN} &&
   composer global require -q drush/drush:^7.0 &&
   drush --version
@@ -18,3 +28,14 @@ git clone -q --depth 5 ${UI_URL} ui &
 
 git config --global user.email "ci@toila.net"
 git config --global user.name "CI"
+
+# ---------------------
+# Node & tools
+# ---------------------
+export CXX=g++-4.8
+nvm install 4 &&
+  npm install -g npm@latest &&
+  npm --version &&
+  npm install -g bower grunt-cli &&
+  gem --version &&
+  gem install sass compass guard-livereload
