14e2b82f5cfd464ace7d17f81bdb440e5ebd9fc0
Andy T <andytruong@users.noreply.github.com>
2015-11-16 15:17:15 +0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -10,3 +10,8 @@ nvm install 4 &&
 composer config -g github-oauth.github.com ${GITHUB_OAUTH_TOKEN} &&
   composer global require -q drush/drush:^7.0 &&
   drush --version
+
+git clone -q --depth 5 $BUILD_URL build &
+git clone -q --depth 5 $MAKE_URL --branch=$MAKE_BRANCH make &
+git clone -q --depth 5 ${ACCOUNTS_URL} accounts &
+git clone -q --depth 5 ${UI_URL} ui &
