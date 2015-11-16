b56c6b89947bb5e77605dab3323bd31b1d8beaba
Andy T <andytruong@users.noreply.github.com>
2015-11-16 15:12:21 +0700

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,7 @@
+export CXX=g++-4.8
+nvm install 4 &&
+  npm install -g npm@latest &&
+    npm --version &&
+    npm install -g bower grunt-cli &&
+    gem --version &&
+    gem install sass compass guard-livereload
