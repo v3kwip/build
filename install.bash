307f618d82400a188a935e59e3cab2fc03bfd297
Andy T <andytruong@users.noreply.github.com>
2015-11-16 15:13:35 +0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,7 +1,11 @@
 export CXX=g++-4.8
+
 nvm install 4 &&
   npm install -g npm@latest &&
     npm --version &&
     npm install -g bower grunt-cli &&
     gem --version &&
     gem install sass compass guard-livereload
+
+composer global require -q drush/drush:^7.0 
+  && drush --version
