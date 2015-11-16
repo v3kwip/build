12a5a35f71410d3851f2fc9adddca2f42dbe4e3f
Andy T <andytruong@users.noreply.github.com>
2015-11-16 15:15:28 +0700

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,10 +2,11 @@ export CXX=g++-4.8
 
 nvm install 4 &&
   npm install -g npm@latest &&
-    npm --version &&
-    npm install -g bower grunt-cli &&
-    gem --version &&
-    gem install sass compass guard-livereload
+  npm --version &&
+  npm install -g bower grunt-cli &&
+  gem --version &&
+  gem install sass compass guard-livereload
 
-composer global require -q drush/drush:^7.0 
-  && drush --version
+composer config -g github-oauth.github.com ${GITHUB_OAUTH_TOKEN} &&
+  composer global require -q drush/drush:^7.0 &&
+  drush --version
