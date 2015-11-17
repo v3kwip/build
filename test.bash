bbfe249ef592545a00e4a0bf32cef78bf3f3945b
Andy T <andytruong@users.noreply.github.com>
2015-11-17 11:56:35 +0700

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,9 +1,7 @@
 # --------
 # Test #accounts
 # --------
-cd ~/clone/accounts &&
-  composer install -q &&
-  # ./vendor/bin/phpunit &&
+# cd ~/clone/accounts && composer install -q && ./vendor/bin/phpunit &&
 
 # --------
 # Build & test #ui
@@ -12,12 +10,8 @@ cd ~/clone/ui &&
   export CXX=g++-4.8 &&
   npm install -q -g npm@latest &&
   npm install -q -g bower grunt-cli &&
-  npm install -q &&
-  bower install -q &&
+  npm install -q && bower install -q &&
   gem install -q sass compass guard-livereload &&
-  grunt set-env:testing &&
-  grunt ngconstant:testing &&
-  grunt build &&
-  grunt jshint &&
-  # grunt test &&
+  grunt set-env:qa && grunt ngconstant:qa &&
+  grunt build && grunt jshint && # grunt test &&
   rm -rf .tmp
